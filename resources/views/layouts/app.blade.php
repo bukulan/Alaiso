c63c2ae3b1ccb50c1850ba80cef7b8d529e4d940
EC2 Default User <ec2-user@ip-172-31-17-170.us-east-2.compute.internal>
2019-01-22 12:30:12 +0000

    CRUD messages

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>MessageBoard for Alaiso</title>
+    </head>
+    
+    <body>
+        @yield('content')
+    </body>
+</html>
